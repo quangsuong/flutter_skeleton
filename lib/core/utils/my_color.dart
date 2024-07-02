b3d0f5e3bebe312bac64f2efec04bb6b8c4f25f3
Suong Nguyen <quangsuongit@gmail.com>
2024-07-02 09:09:08 +0700

    Add splash screen

---
+++ lib/core/utils/my_color.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static Color gbr = Colors.black.withOpacity(0.3);
+  static Color textFiledFillColor = Colors.grey[600]!.withOpacity(0.3);
+  static const Color t1 = Color(0xFFFEFCFF);
+  static const Color t2 = Color(0xFFB5B4BC);
+  static const Color t3 = Color(0xFF9B9CA6);
+  static const Color t4 = Color(0xFF383844);
+  static const Color colorGrey = Color(0xFFA0A4A8);
+  static const Color otpBgColor = Color(0xFF172B33);
+  static const Color cardBg = Color(0xFF282934);
+  static const Color colorBlack = Color(0xFF000000);
+  static const Color colorWhite = t1;
+  static const Color colorHint = Color(0xFF52575C);
+  static const Color colorGrey2 = Color(0xff6E6E6E);
+  static const Color colorBlack2 = Color(0xff25282B);
+  static const Color colorGrey3 = Color(0xffF1F3F4);
+  static const Color placeHolderColor = Color(0xff42434D);
+  static Color transparentColor = const Color(0xff131722);
+
+  static const primaryColor = Color(0xFFD50055);
+  static Color primaryColor100 = primaryColor.withOpacity(.9);
+  static Color primaryColor200 = primaryColor.withOpacity(.8);
+  static Color primaryColor300 = primaryColor.withOpacity(.7);
+  static Color primaryColor350 = primaryColor.withOpacity(.6);
+  static Color primaryColor400 = primaryColor.withOpacity(.5);
+  static Color primaryColor500 = primaryColor.withOpacity(.3);
+  static Color primaryColor900 = primaryColor.withOpacity(.1);
+
+  static const Color secondaryColor = Color(0xff1a1d1f);
+  static const Color bodyTextColor = Color(0xFF747475);
+  static const Color borderColor = Color(0xFF747475);
+  static const Color textFieldColor = Color(0xFF2E303A);
+
+  static const Color shimmerBaseColor = Color(0xFF2A2E38);
+  static const Color shimmerSplashColor = Color(0xFF52575C);
+
+  static const Color highPriorityPurpleColor = Color(0xFF7367F0);
+  static const Color bgColor = Color(0xFF272834);
+
+  static const Color closeRedColor = Color(0xFFEA5455);
+  static const Color greenSuccessColor = Color(0xFF28C76F);
+  static const Color redCancelTextColor = Color(0xFFF93E2C);
+  static const Color lightOrange = Color(0xFFFAB598);
+
+  static const Color primaryText2 = Color(0xFFE4E8EC);
+  static const Color hintTextColor = Color(0xFF98a1ab);
+  static const Color textColor = Color(0xffa9aaac);
+  static const primaryText = Color(0xffa9aaac);
+
+  static const Color red = Color(0xFFD92027);
+  static const Color fbColor = Color(0xFF0F8AE6);
+  static const Color gmailColor = colorHint;
+}
